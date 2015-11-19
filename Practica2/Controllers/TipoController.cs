1305a19766ab0e8a4cd2c15185d767c095b535b2
Felix <martinez.alvaro.felix@gmail.com>
2015-11-19 12:32:09 +0100

    PracticaTaller

---
+++ Practica2/Controllers/TipoController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practica2.Models;
+
+namespace Practica2.Controllers
+{
+    public class TipoController : Controller
+    {
+        TallerEntities db = new TallerEntities();
+        // GET: Tipo
+        public ActionResult Index()
+        {
+            var data = db.Tipo;
+            return View(data);
+        }
+
+        public ActionResult Alta()
+        {
+            return View(new Tipo());
+        }
+
+        [HttpPost]
+        public ActionResult Alta(Tipo model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tipo.Add(model);
+                db.SaveChanges();
+                return View(new Tipo());
+            }
+            return View(model);
+        }
+
+        public ActionResult Modificar(int id)
+        {
+            var data = db.Tipo.Find(id);
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Modificar(Tipo model)
+        {
+            if (ModelState.IsValid)
+            {
+                var data = db.Tipo.FirstOrDefault(o => o.idTipo == model.idTipo);
+                data.nombre = model.nombre;
+                data.descripcion = model.descripcion;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Borrar(int id)
+        {
+            var data = db.Tipo.Find(id);
+            if (data.Vehiculo.Any())
+                db.Vehiculo.RemoveRange(data.Vehiculo);
+            db.Tipo.Remove(data);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Practica2/Controllers/VehiculoController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practica2.Models;
+
+namespace Practica2.Controllers
+{
+    public class VehiculoController : Controller
+    {
+        TallerEntities db = new TallerEntities();
+        // GET: Vehiculo
+        public ActionResult Index(int id)
+        {
+            ViewBag.idTipo = id;
+            return View(db.Vehiculo.Where(o => o.idTipo == id));
+        }
+
+        [OutputCache(Duration = 0, VaryByParam = "*")]
+        public ActionResult Buscar(String busqueda, int id, String filtro)
+        {
+            IQueryable<Vehiculo> data;
+            if (busqueda.Equals(""))
+            {
+                data = db.Vehiculo.Where(o => o.idTipo == id);
+            }
+            else
+            {
+                if (filtro.Equals("Marca"))
+                    data = db.Vehiculo.Where(o => o.marca.Contains(busqueda) && o.idTipo == id);
+                else if (filtro.Equals("Matricula"))
+                    data = db.Vehiculo.Where(o => o.matricula.Contains(busqueda) && o.idTipo == id);
+                else
+                {
+                    data = null;
+                }
+            }
+
+
+            return PartialView("_listadoVehiculo", data);
+        }
+
+        [HttpPost]
+        public ActionResult Alta(Vehiculo model)
+        {
+            db.Vehiculo.Add(model);
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return Json(model);
+        }
+    }
+}
